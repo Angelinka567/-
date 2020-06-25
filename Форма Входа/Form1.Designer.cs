b2b923dbd3116f78f6f4031b6a767ec0ddfaae5b
Ангелина Лосева <angelina.loseva@list.ru>
2020-06-25 21:00:29 +0500

    Smth has changed

---
+++ Форма Входа/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Форма_Входа
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.nameLogin = new System.Windows.Forms.TextBox();
+            this.namePassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(102, 245);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Авторизация";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nameLogin
+            // 
+            this.nameLogin.Location = new System.Drawing.Point(221, 103);
+            this.nameLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.nameLogin.Name = "nameLogin";
+            this.nameLogin.Size = new System.Drawing.Size(148, 27);
+            this.nameLogin.TabIndex = 1;
+            // 
+            // namePassword
+            // 
+            this.namePassword.Location = new System.Drawing.Point(221, 147);
+            this.namePassword.Margin = new System.Windows.Forms.Padding(4);
+            this.namePassword.Name = "namePassword";
+            this.namePassword.PasswordChar = '*';
+            this.namePassword.Size = new System.Drawing.Size(148, 27);
+            this.namePassword.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Light", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 38);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите код пользователя(логин)\r\nи пароль для входа в систему.\r\n";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 245);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 33);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Параметры";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 82);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 38);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Код пользователя\r\n(логин):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 155);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Пароль:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.checkBox1.Location = new System.Drawing.Point(65, 194);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(154, 24);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Запомнить меня";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.nameLogin);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.namePassword);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(374, 284);
+            this.panel1.TabIndex = 10;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 304);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Bahnschrift Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Вход в систему";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nameLogin;
+        private System.Windows.Forms.TextBox namePassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Форма Входа/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Форма_Входа
+{
+    public partial class Form1 : Form
+    {
+        int try1 = 0;//количество попыток входа в систему
+        public Form1()
+        {
+            InitializeComponent();
+            if(Properties.Settings.Default.login.Length!=0 && Properties.Settings.Default.password.Length != 0)
+            {
+                nameLogin.Text = Properties.Settings.Default.login;
+                namePassword.Text = Properties.Settings.Default.password;
+                checkBox1.Checked = true;
+            }
+            if (Properties.Settings.Default.ThisTry > 0)
+            {
+                //блокировка всех кнопок, если время ожидание вледующей попытки еще не истекло
+                timer1.Enabled = true;
+                checkBox1.Enabled = false;
+                button1.Enabled = false;
+                button3.Enabled = false;
+                nameLogin.Enabled = false;
+                namePassword.Enabled = false;
+            }
+            else
+            {
+                //активация всех кнопок, если время ожидания следующей попытки истелко
+                checkBox1.Enabled = true;
+                timer1.Enabled = false;
+                button1.Enabled = true;
+                button3.Enabled = true;
+                nameLogin.Enabled = true;
+                namePassword.Enabled = true;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (try1 == 3 | Properties.Settings.Default.ThisTry > 0)//проверка кол-ва попыток входа
+            {
+                Properties.Settings.Default.ThisTry = 60;
+                //переменная, которая сохраняется в системе, даже после ее перезагрузки
+                Properties.Settings.Default.Save();
+                //сохраняем ее значения
+
+                timer1.Enabled = true;
+
+                button1.Enabled = false;
+                //если попыток больше 3 раз, то система блакирует кнопки и текстовые поля
+                checkBox1.Enabled = false;
+                button3.Enabled = false;
+                nameLogin.Enabled = false;
+                namePassword.Enabled = false;
+            }
+
+
+            string login = nameLogin.Text.Trim();
+            string password = namePassword.Text.Trim();
+            if (checkBox1.Checked)
+            {
+                Properties.Settings.Default.login = login;
+                Properties.Settings.Default.password = password;
+                Properties.Settings.Default.Save();
+            }
+            if (login=="" | password=="")
+            {
+                MessageBox.Show("Некоторые поля не заполнены!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if((login.Equals("login1") && password.Equals("pas1")) | (login.Equals("login2") && password.Equals("pas2")) | (login.Equals("login3") && password.Equals("pas3")))
+                {
+                    MessageBox.Show("Добро пожаловать в несуществующую систему!", "Несуществующая система приветсвует вас", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    Form2 n = new Form2();
+                    n.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь не найден", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    try1++;
+                }
+                nameLogin.Text = "";
+                namePassword.Text = "";
+            }
+            
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //таймер, отображающий отсчёт времени
+            
+            button1.Text = (Properties.Settings.Default.ThisTry--) + "сек..";
+            Properties.Settings.Default.Save();
+
+            if (Properties.Settings.Default.ThisTry == -2)//если время истекло, то активирует все кнопки
+            {
+                timer1.Enabled = false;
+                try1 = 1;
+                button1.Text = "Авторизация";
+                checkBox1.Enabled = true;
+                button1.Enabled = true;
+                button3.Enabled = true;
+                nameLogin.Enabled = true;
+                namePassword.Enabled = true;
+            }
+
+        }
+
+        private void messageQueue1_ReceiveCompleted(object sender, System.Messaging.ReceiveCompletedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
